f808a54a40fd0d684eed638825ccbd6a0616b01d
German Perov <ggmusya@gmail.com>
2026-02-15 12:16:23 +0300

    build: use fetchcontent for dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,2 +1,19 @@
-find_package(GTest REQUIRED)
-find_package(benchmark REQUIRED)
+include(FetchContent)
+
+FetchContent_Declare(
+  googletest
+  SYSTEM
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        v1.17.0
+)
+
+FetchContent_MakeAvailable(googletest)
+
+FetchContent_Declare(
+  benchmark
+  SYSTEM
+  GIT_REPOSITORY https://github.com/google/benchmark.git
+  GIT_TAG        v1.9.4
+)
+
+FetchContent_MakeAvailable(benchmark)
