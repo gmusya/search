7ae06c200b2c9e09531702e3a0d9d898c74744de
German Perov <ggmusya@gmail.com>
2026-02-15 15:04:19 +0300

    fix: add flush to write

--- src/bytes.h
+++ src/bytes.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <algorithm>
+#include <bit>
 #include <cassert>
 #include <cstdint>
 #include <cstring>
--- src/local_file.h
+++ src/local_file.h
@@ -13,7 +13,7 @@ namespace search {
 class LocalFile : public IFile {
  public:
   LocalFile(const std::string& file_path)
-      : file_path_(file_path), file_(file_path, std::ios_base::in | std::ios_base::out) {}
+      : file_path_(file_path), file_(file_path, std::ios_base::in | std::ios_base::out | std::ios_base::binary) {}
 
   void Read(uint64_t offset, uint64_t bytes, void* output) const override {
     file_.seekp(offset);
@@ -23,6 +23,7 @@ class LocalFile : public IFile {
   void Write(uint64_t offset, const void* data, uint64_t size) override {
     file_.seekp(offset);
     file_.write(reinterpret_cast<const char*>(data), size);
+    file_.flush();
   }
 
   void Resize(uint64_t size) override { std::filesystem::resize_file(file_path_, size); }
